cd8e67ae5bad760ac8849b0a2b24504f8218c8ea
Cody Anderson <cody-anderson@uiowa.edu>
2020-03-20 17:06:02 -0500

    Added the very basics of the Molecule class implementation.

---
+++ Chemical_Equations_Solver/Molecule.swift
@@ -0,0 +1,28 @@
+//
+//  Molecule.swift
+//  Chemical_Equations_Solver
+//
+//  Created by Cody Anderson on 3/20/20.
+//  Copyright © 2020 Cody Anderson. All rights reserved.
+//
+
+import Foundation
+
+class Molecule {
+    
+    let name: String
+    var moleculeData: Dictionary<String, Int>
+    
+    init(molName: String){
+        
+        self.name = molName
+        moleculeData = [:]
+        
+    }
+    
+}
+
+
+
+
+
