9a7b03879ad9cd7ee11a69063698b7f5d1608a91
Cody Anderson <cody-anderson@uiowa.edu>
2020-03-21 09:23:34 -0500

    Added the core functionality for the Molecule class.

--- Chemical_Equations_Solver/Molecule.swift
+++ Chemical_Equations_Solver/Molecule.swift
@@ -20,6 +20,51 @@ class Molecule {
         
     }
     
+    // Note that if the key you are trying to acess doesnt exist, the dictionary returns nil
+    func addElement(element:String, multiplicity:Int){
+        if(moleculeData[element] == nil){
+            moleculeData[element] = multiplicity
+        } else {
+            print("Error! That element is already in the molecule!")
+        }
+    }
+    
+    
+    // Note that if the key you are trying to acess doesnt exist, the dictionary returns nil
+    func incrementElement(element:String, multiplicity:Int){
+        let num = moleculeData[element]
+        
+        if (num != nil){
+            moleculeData[element] = num! + multiplicity
+        } else {
+            print("Error! the key you entered does not exist.")
+        }
+    }
+    
+    
+    // Note that if the key you are trying to acess doesnt exist, the dictionary returns nil
+    func contains(element:String) -> Bool {
+        let testVar = moleculeData[element]
+        
+        if (testVar != nil) {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    // Note that if the key you are trying to acess doesnt exist, the dictionary returns nil
+    func multiplicity(element:String) -> Int {
+        
+        let num = moleculeData[element]
+        
+        if (num != nil){
+            return num!
+        } else {
+            return 0
+        }
+    }
+    
 }
 
 
