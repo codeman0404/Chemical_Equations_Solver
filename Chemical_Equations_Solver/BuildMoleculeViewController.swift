136b23e8986961545262689d40632d6fbc8979ea
Cody Anderson <cody-anderson@uiowa.edu>
2020-03-26 15:04:59 -0500

    bug fixes and other related stuff.

---
+++ Chemical_Equations_Solver/BuildMoleculeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BuildMoleculeViewController.swift
+//  Chemical_Equations_Solver
+//
+//  Created by Cody Anderson on 3/21/20.
+//  Copyright © 2020 Cody Anderson. All rights reserved.
+//
+
+import UIKit
+
+class BuildMoleculeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Chemical_Equations_Solver/ViewController.swift
+++ Chemical_Equations_Solver/ViewController.swift
@@ -9,6 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var rightSideEqLabel: UILabel!
+    @IBOutlet weak var leftSideEqLabel: UILabel!
+    
+    // This button takes whatever molecule the slider has currently selected and adds it to the left side of the equation
+    @IBAction func addMoleculeRight(_ sender: Any) {
+        
+        
+    }
+    
+    
+    
+    
+    // This button takes whatever molecule the slider has currently selected and adds it to the left side of the equation
+    @IBAction func addMoleculeLeft(_ sender: Any) {
+        
+        
+    }
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,13 +46,21 @@ class ViewController: UIViewController {
         let HisIn = myMolecule.contains(element: "H")
         print(OisIN)
         print(HisIn)
-        print(myMolecule.moleculeData)
         myMolecule.addElement(element: "H", multiplicity: 2)
         myMolecule.incrementElement(element: "W", multiplicity: 25)
         let mult = myMolecule.getMultiplicity(element: "H")
         let mult2 = myMolecule.getMultiplicity(element: "@")
         print(mult)
         print(mult2)
+        
+    
+        // This is example code for how to create a string with subscripts
+        let font:UIFont? = UIFont(name: "Helvetica", size:20)
+        let fontSub:UIFont? = UIFont(name: "Helvetica", size:10)
+        let attString:NSMutableAttributedString = NSMutableAttributedString(string: "H2O", attributes: [.font:font!])
+        attString.setAttributes([.font:fontSub!,.baselineOffset:-3], range: NSRange(location:1,length:1))
+        rightSideEqLabel.attributedText = attString
+        
     }
 
 
