2cb20f0e9cf8682c56cbc266d9a84fb72fc77247
Cody Anderson <cody-anderson@uiowa.edu>
2020-03-28 11:29:43 -0500

    Wrote string parsing method to turn a string into a molecule object. Note that the function does not check whether the sting entered is a valid molecule or not.

--- Chemical_Equations_Solver/ViewController.swift
+++ Chemical_Equations_Solver/ViewController.swift
@@ -10,13 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var rightSideEqLabel: UILabel!
     @IBOutlet weak var leftSideEqLabel: UILabel!
     
     // This button takes whatever molecule the slider has currently selected and adds it to the left side of the equation
     @IBAction func addMoleculeRight(_ sender: Any) {
         
+        let moleculeName = textField.text
         
+        if (moleculeName != ""){
+            
+            let mol: Molecule
+            
+            mol = convertStringToMolecule(moleculeString: moleculeName!)
+            
+            rightMolecules.append(mol)
+            
+        }
     }
     
     
@@ -25,9 +36,113 @@ class ViewController: UIViewController {
     // This button takes whatever molecule the slider has currently selected and adds it to the left side of the equation
     @IBAction func addMoleculeLeft(_ sender: Any) {
         
+        let moleculeName = textField.text
+        
+        if (moleculeName != ""){
+            
+            let mol: Molecule
+            
+            mol = convertStringToMolecule(moleculeString: moleculeName!)
+            
+            leftMolecules.append(mol)
+            
+        }
+        
         
     }
     
+    // This button ballances the currently displayed equation.
+    @IBAction func balanceEquation(_ sender: Any) {
+        
+    }
+    
+    
+    // Define Molecule arrays to keep track of the molecules on each side of the equation
+    var leftMolecules: [Molecule] = []
+    var rightMolecules: [Molecule] = []
+    
+    
+    // Function description: convertStringToMolecule takes in a string and processes its information. This information is stored and returned as a molecule Object.
+    func convertStringToMolecule(moleculeString:String) -> Molecule {
+        
+        let mol = Molecule(molName: moleculeString)
+        
+        
+        // instantiate variables for string traversal
+        var i = 0
+        let length = moleculeString.count
+        
+        var moleculeData: Dictionary<String, Int>
+        moleculeData = [:]
+        let charArray = Array(moleculeString)
+        
+        while (i < (length - 1)){
+            
+            let element = String(charArray[i])
+            
+            // If the next element is a number, Do this.
+            if let multiplicity = Int(String(charArray[i+1])) {
+                
+                moleculeData[element] = multiplicity
+                
+                // increment by 2 to skip the multiplicity
+                i = i + 2
+                
+            // The element does not have a multiplicity after it
+            } else {
+                
+                let testForExistence = moleculeData[element]
+                
+                // if the atom already exists, increment it by 1
+                if (testForExistence != nil){
+                    
+                    moleculeData[element] = moleculeData[element]! + 1
+                 
+                // if the atom doesnt exist, add it to the molecule
+                } else {
+                    moleculeData[element] = 1
+                }
+                
+                // increment by 1 as normal
+                i = i + 1
+            }
+            
+        }
+        
+        // Take care of last pass through loop where you know that you cannot be on a number
+        if (i < length){
+            let element = String(charArray[i])
+        
+            let testForExistence = moleculeData[element]
+        
+            // if the atom already exists, increment it by 1
+            if (testForExistence != nil){
+                
+                moleculeData[element] = moleculeData[element]! + 1
+                
+            // if the atom doesnt exist, add it to the molecule
+            } else {
+                moleculeData[element] = 1
+            }
+        }
+        
+        
+        
+        // add all of the atoms and their multiplicities to mol
+        for key in moleculeData.keys {
+            mol.addElement(element: key, multiplicity: moleculeData[key]!)
+        }
+        
+        
+        // return the generated molecule
+        return mol
+    }
+    
+    
+    // helper function that updates the equation labels
+    func updateGraphics(){
+        
+    }
     
     
 
@@ -36,9 +151,10 @@ class ViewController: UIViewController {
         
         
         // Do any additional setup after loading the view.
+    
         
-        
-        var myMolecule = Molecule(molName: "H2O")
+        // Test Code for the molecule class.
+        let myMolecule = Molecule(molName: "H2O")
         myMolecule.addElement(element: "H", multiplicity: 1)
         myMolecule.incrementElement(element: "H", multiplicity: 1)
         myMolecule.addElement(element: "O", multiplicity: 1)
